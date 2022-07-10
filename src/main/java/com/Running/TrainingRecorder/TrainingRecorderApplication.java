6a3dee7e4dd154c46236910dab6c010ad5aa3d3a
Konrad <konx90@gmail.com>
2022-07-10 21:16:24 +0200

    application initialization

---
+++ src/main/java/com/Running/TrainingRecorder/TrainingRecorderApplication.java
@@ -0,0 +1,13 @@
+package com.Running.TrainingRecorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingRecorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingRecorderApplication.class, args);
+	}
+
+}
